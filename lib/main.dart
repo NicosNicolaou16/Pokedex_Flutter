fdc08afb7af413a3f723ed683ae6c0020b4e2944
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-05-03 01:42:21 +0300

    init directories and database

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'my_app.dart';
+import 'views/my_app.dart';
 
 void main() {
   runApp(const MyApp());
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pokedex_flutter/my_app.dart';
+import 'package:pokedex_flutter/views/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
