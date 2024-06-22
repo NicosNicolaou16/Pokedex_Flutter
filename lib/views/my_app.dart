4c8294fb7a7c3eaa3de0efa9d58b2b23fadbbe34
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-06-22 22:00:11 +0300

    update my library

--- lib/views/my_app.dart
+++ lib/views/my_app.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pokedex Flutter',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
